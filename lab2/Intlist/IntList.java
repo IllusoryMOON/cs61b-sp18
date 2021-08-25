28b3c87babac1ef9aa2a114790aa6c801dbe2deb
IllusoryMOON <88801840+IllusoryMOON@users.noreply.github.com>
2021-08-25 20:00:23 +0800

    test null arguments

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -99,6 +99,9 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null) {
+            return B;
+        }
         IntList AB = new IntList(A.first, null);
         IntList p1 = A;
         IntList p2 = AB;
